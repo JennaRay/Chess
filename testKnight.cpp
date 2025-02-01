54b253fcb4c4353b6675267fbe873de7ecb44ac3
Jenna Ray <ray19008@byui.edu>
2025-02-01 15:15:57 -0700

    all test cases implimented

--- testKnight.cpp
+++ testKnight.cpp
@@ -71,7 +71,44 @@ void TestKnight::getMoves_end()
  **************************************/
 void TestKnight::getMoves_blocked()
 {
-   assertUnit(NOT_YET_IMPLEMENTED);
+   //setup
+   BoardEmpty board;
+   Knight knight(7, 7, false /*white*/);
+   knight.fWhite = true;
+   knight.position.set(3, 4);
+   White white5(PAWN);
+   White white6(PAWN);
+   White white7(PAWN);
+   White white8(PAWN);
+   board.board[1][5] = &white6;
+   board.board[2][6] = &white7;
+   board.board[4][6] = &white8;
+   board.board[5][5] = &white5;
+   White white1(PAWN);
+   White white2(PAWN);
+   White white3(PAWN);
+   White white4(PAWN);
+   board.board[1][3] = &white1;
+   board.board[2][2] = &white2;
+   board.board[4][2] = &white3;
+   board.board[5][3] = &white4;
+   set <Move> moves;
+
+   //exercise
+   knight.getMoves(moves, board);
+
+   //verify
+   assertUnit(moves.size() == 0);  // no possible moves
+
+   //teardown
+   board.board[1][5] = nullptr; // black pawn
+   board.board[2][6] = nullptr; // black pawn
+   board.board[4][6] = nullptr; // black pawn
+   board.board[5][5] = nullptr; // black pawn
+   board.board[1][3] = nullptr; // white pawn
+   board.board[2][2] = nullptr; // white pawn
+   board.board[4][2] = nullptr; // white pawn
+   board.board[5][3] = nullptr; // white pawn
 }
 
 /*************************************
@@ -90,7 +127,53 @@ void TestKnight::getMoves_blocked()
  **************************************/
 void TestKnight::getMoves_capture()
 {
-   assertUnit(NOT_YET_IMPLEMENTED);
+   //setup
+   BoardEmpty board;
+   Knight knight(7, 7, false /*white*/);
+   knight.fWhite = true;
+   knight.position.set(3, 4);
+   Black black1(PAWN);
+   Black black2(PAWN);
+   Black black3(PAWN);
+   Black black4(PAWN);
+   board.board[1][5] = &black1;
+   board.board[2][6] = &black2;
+   board.board[4][6] = &black3;
+   board.board[5][5] = &black4;
+   Black black5(PAWN);
+   Black black6(PAWN);
+   Black black7(PAWN);
+   Black black8(PAWN);
+   board.board[1][3] = &black5;
+   board.board[2][2] = &black6;
+   board.board[4][2] = &black7;
+   board.board[5][3] = &black8;
+   set <Move> moves;
+
+   //exercise
+   knight.getMoves(moves, board);
+
+   //verify
+   assertUnit(moves.size() == 8);  // many possible moves
+   assertUnit(moves.find(Move("d5b6p")) != moves.end());
+   assertUnit(moves.find(Move("d5c7p")) != moves.end());
+   assertUnit(moves.find(Move("d5e7p")) != moves.end());
+   assertUnit(moves.find(Move("d5f6p")) != moves.end());
+   assertUnit(moves.find(Move("d5b4p")) != moves.end());
+   assertUnit(moves.find(Move("d5c3p")) != moves.end());
+   assertUnit(moves.find(Move("d5e3p")) != moves.end());
+   assertUnit(moves.find(Move("d5f4p")) != moves.end());
+
+   //teardown
+   board.board[1][5] = nullptr; // black pawn
+   board.board[2][6] = nullptr; // black pawn
+   board.board[4][6] = nullptr; // black pawn
+   board.board[5][5] = nullptr; // black pawn
+   board.board[1][3] = nullptr; // black pawn
+   board.board[2][2] = nullptr; // black pawn
+   board.board[4][2] = nullptr; // black pawn
+   board.board[5][3] = nullptr; // black pawn
+
 }
 
 /*************************************
@@ -109,7 +192,26 @@ void TestKnight::getMoves_capture()
  **************************************/
 void TestKnight::getMoves_free()
 {
-   assertUnit(NOT_YET_IMPLEMENTED);
+   //setup
+   BoardEmpty board;
+   Knight knight(7, 7, false /*white*/);
+   knight.fWhite = true;
+   knight.position.set(3, 4);
+   set <Move> moves;
+
+   //exercise
+   knight.getMoves(moves, board);
+
+   //verify
+   assertUnit(moves.size() == 8);  // many possible moves
+   assertUnit(moves.find(Move("d5b6")) != moves.end());
+   assertUnit(moves.find(Move("d5c7")) != moves.end());
+   assertUnit(moves.find(Move("d5e7")) != moves.end());
+   assertUnit(moves.find(Move("d5f6")) != moves.end());
+   assertUnit(moves.find(Move("d5b4")) != moves.end());
+   assertUnit(moves.find(Move("d5c3")) != moves.end());
+   assertUnit(moves.find(Move("d5e3")) != moves.end());
+   assertUnit(moves.find(Move("d5f4")) != moves.end());
 }
 
 
